24eebdff44e6aa95ee78cef890656d1d5e6b4bd7
valery <valery@MacBook-Pro-Valerie.local>
2023-05-23 13:29:09 +0300

    new12

--- homework07/hackernews.py
+++ homework07/hackernews.py
@@ -3,7 +3,7 @@ import sqlite3
 from bottle import redirect, request, route, run, template
 from db import News, session
 from scraputils import get_news
-from typing import List, Optional
+
 
 @route("/news")
 def news_list() -> str:
