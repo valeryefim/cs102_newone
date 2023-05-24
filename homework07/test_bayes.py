56f25c9a285ca22cabe175f4cf8984d0fcfad654
valery <valery@MacBook-Pro-Valerie.local>
2023-05-24 18:34:53 +0300

    pravki6

--- homework07/test_bayes.py
+++ homework07/test_bayes.py
@@ -4,7 +4,7 @@ import unittest
 
 from bayes import NaiveBayesClassifier
 
-PATH_FOR_GIT = "/home/runner/work/cs102/cs102/homework07/data"
+PATH_FOR_GIT = "/home/runner/work/cs102/cs102/homework07"
 
 
 class TestBayes(unittest.TestCase):
