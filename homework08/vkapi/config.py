f436718c320d0eaa33d65e0f7ccd9fed7b210528
valery <valery@MacBook-Pro-Valerie.local>
2023-06-09 16:56:46 +0300

    try 15

--- homework08/vkapi/config.py
+++ homework08/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "vk1.a.0qnqZQOT6tGRjNCNccIwAcJ534dUHuQe5gx2SCwiZS7luoUqZVvb3b-kSMkmLhJPo0CsBoamLuwxn01W2nAIgQureervynVeJFm0aygVkJcxaOnz71dA-EstqwCiqRXZiiJ5N-1g4l04tqI-ewhYBTMwdDS11I9CpJ7XPz9C7cEHWCtem-vTN9uE95QSkbqN",
     "version": "5.126",
 }
