617d017f3dd8dc88cfe339d1079ab97f969e5273
valery <valery@MacBook-Pro-Valerie.local>
2023-06-09 17:41:50 +0300

    try 21

--- homework08/vkapi/friends.py
+++ homework08/vkapi/friends.py
@@ -95,6 +95,7 @@ def get_mutual(
                 mut_friends_for_list.extend(friend_friends)
             except:
                 continue
+        source_uid = [source_uid]
         mutual_friends = list(set(source_uid).intersection(mut_friends_for_list))
 
     return mutual_friends
