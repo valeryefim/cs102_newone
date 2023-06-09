da1bbb2200fb013221fba39e58702df8b4e8ee30
valery <valery@MacBook-Pro-Valerie.local>
2023-06-09 15:36:39 +0300

    try 9

--- homework08/tests/tests_api/test_session.py
+++ homework08/tests/tests_api/test_session.py
@@ -3,7 +3,7 @@ import unittest
 
 import httpretty
 import responses
-from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError
+from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError  # type: ignore
 from vkapi.session import Session
 
 
